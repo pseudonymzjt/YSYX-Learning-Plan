43239c6d5c98ba8e31e7b76838dafb33b250b5e3
Zhou JIntian <zhoujintian25@mails.ucas.ac.cn>
2026-08-30 15:15:50 +0800

    implement add

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -14,18 +14,20 @@ module top(
 	// 00000000 <_start>:
 		// 1. addi	a0,zero,20
 		inst_mem[0] = 32'h01400513; 
-		// 2. jalr	ra,16(zero) # 10 <fun>
-		inst_mem[1] = 32'h010000e7; 
-		// 3. jalr	ra,12(zero) # c <halt>
-		inst_mem[2] = 32'h00c000e7; 
-	// 0000000c <halt>:
-		// 4. ebreak
-		inst_mem[3] = 32'h00100073;
-	// 00000010 <fun>:
-		// 5. addi	a0,a0,10
-		inst_mem[4] = 32'h00a50513;
-		// 6. jalr	zero,0(ra)
-		inst_mem[5] = 32'h00008067;
+		// 2. add  a0,a0,a0
+		inst_mem[1] = 32'h00a50533;
+		// 3. jalr	ra,16(zero) # 14 <fun>
+		inst_mem[2] = 32'h014000e7; 
+		// 4. jalr	ra,12(zero) # 10 <halt>
+		inst_mem[3] = 32'h010000e7; 
+	// 00000010 <halt>:
+		// 5. ebreak
+		inst_mem[4] = 32'h00100073;
+	// 00000014 <fun>:
+		// 6. addi	a0,a0,10
+		inst_mem[5] = 32'h00a50513;
+		// 7. jalr	zero,0(ra)
+		inst_mem[6] = 32'h00008067;
 	end
 	wire [31:0] inst = inst_mem[pc[5:2]];
 
@@ -35,6 +37,9 @@ module top(
 	wire [2:0] funct3 = inst[14:12];
 	wire [4:0] rs1 = inst[19:15];
 	wire [11:0] imm = inst[31:20];
+
+	// R 型指令
+	wire [6:0] funct7 = inst[31:25];
 
 	wire [4:0] rs2 = inst[24:20];
 	// 实例化读写模块
@@ -49,7 +54,7 @@ module top(
 		.waddr(rd),
 		.raddr1(rs1),
 		.raddr2(rs2),
-		.wen(opcode == 7'b0010011 || opcode == 7'b1100111),
+		.wen(opcode == 7'b0010011 || opcode == 7'b1100111 || opcode == 7'b0110011),
 		.rdata1(rdata1),
 		.rdata2(rdata2)
 	);
@@ -59,11 +64,15 @@ module top(
 		alu_result = 32'b0;
 		case(opcode)
 			7'b0010011: begin
-				alu_result = rdata1 + imm_ext;
+				alu_result = rdata1 + imm_ext; // addi
 			end
 			7'b1100111: begin
 				alu_result = pc + 4;
-				next_pc = (rdata1 + imm_ext) & 32'hfffffffe;
+				next_pc = (rdata1 + imm_ext) & 32'hfffffffe; // jalr
+			end
+			7'b0110011: begin
+				if(funct7 == 7'b0) alu_result = rdata1 + rdata2; // add
+				else if(funct7 == 7'b0100000) alu_result = rdata1 - rdata2; // sub
 			end
 			default: begin
 			end
